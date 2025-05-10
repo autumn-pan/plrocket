38441b961f2f59b498428f7884791146e0124a33
Autumn Pan <pan662607@gmail.com>
2025-05-10 13:09:40 -0400

    add ThrustReverser class

--- src/subsystems/propulsion.cpp
+++ src/subsystems/propulsion.cpp
@@ -36,4 +36,48 @@ boolean Reigniter::update()
             return true;
         }
     }
+    return false;
+}
+
+
+// Thrust Reverser code
+
+ThrustReverser::ThrustReverser(Rocket * rocket, int pin1, int pin2, float initialAngle)
+{
+    this->rocket = rocket;
+    this->pin1 = pin1;
+    this->pin2 = pin2;
+
+    this->servo1.attach(pin1);
+    this->servo2.attach(pin2);
+
+    this->angle = initialAngle;
+
+    this->servo1.write(initialAngle);
+    this->servo2.write(180 - initialAngle);
+}
+
+void ThrustReverser::initReverse()
+{
+    this->isActive = true;
+}
+
+float ThrustReverser::getAngle()
+{
+    return this->angle;
+}
+
+void ThrustReverser::stopReverse()
+{
+    this->isActive = false;
+}
+
+void ThrustReverser::update(float angle)
+{
+    if(!this->isActive)
+        return;
+    this->angle = angle;
+    this->servo1.write(this->angle);
+    this->servo2.write(180 - this->angle);
+
 }
--- src/subsystems/propulsion.h
+++ src/subsystems/propulsion.h
@@ -1,6 +1,8 @@
 #ifndef PROPULSION_H
 #define PROPULSION_H
 #include "../rocket/rocket.h"
+#include <Servo.h>
+
 class Reigniter
 {
     public:
@@ -12,9 +14,38 @@ class Reigniter
         Rocket * rocket;
         boolean state;
 
-        int initialTime;
+        unsigned int initialTime;
         int pin;
-        int duration;
+        unsigned int duration;
+};
+
+
+// Uses a bucket-type thrust reverser as a braking mechanism for the SRMs
+// Works by blocking the exhaust with two servos
+class ThrustReverser
+{
+    public:
+        ThrustReverser(Rocket * rocket, int pin1, int pin2, float initialAngle);
+        // Activates TR
+        void initReverse();
+        // Update TR angle; to be run every iteration
+        void update(float angle);
+        // Deactivates TR
+        void stopReverse();
+
+        // boolean isActive();
+        float getAngle(); // Returns TR angle
+
+    private:
+        Rocket * rocket;
+        unsigned int initialTime;
+        int pin1;
+        int pin2;
+        float angle;
+        boolean isActive;
+
+        Servo servo1;
+        Servo servo2;
 };
 
 #endif
