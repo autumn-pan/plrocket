0b98e32558a5686610e0d9dda3363a69dfa6d327
Autumn Pan <pan662607@gmail.com>
2025-05-13 21:36:46 -0400

    polish sensor class

--- src/subsystems/sensors.cpp
+++ src/subsystems/sensors.cpp
@@ -2,17 +2,20 @@
 #include <Wire.h>
 #include <Adafruit_BNO055.h>
 
-
+// COnstructor for SensorStack
 SensorStack::SensorStack(double seaLevelPressure)
 {
+    // Sets local sea level pressure
     this->seaLevelPressure = seaLevelPressure;
 
+    // Begins I2C
     Wire.begin();
     Wire1.begin();
 
     Wire.setClock(400000);
     Wire1.setClock(400000);
 
+    // Begins 3 IMUs
     if(!bno1.begin())
         Serial.print("BNO055 Unit 1 failed to begin!");
 
@@ -22,9 +25,11 @@ SensorStack::SensorStack(double seaLevelPressure)
     if(!bno3.begin())
         Serial.print("BNO055 Unit 3 failed to begin!");
 
-    if(!bmp.begin_I2C()) // Begin Altimeter
+    // Begins barometer
+    if(!bmp.begin_I2C(0x77, &Wire1)) // Begin Altimeter
         Serial.print("BMP390 failed to begin!");
-    // Configure altimeter        
+
+    // Configures IMUs
     bno1.setExtCrystalUse(true);
     bno2.setExtCrystalUse(true);
     bno3.setExtCrystalUse(true);
@@ -33,12 +38,14 @@ SensorStack::SensorStack(double seaLevelPressure)
 // Update measured sensor values
 void SensorStack::update()
 {
+    // Retrieve angular velocity vectors
     imu::Vector<3> angVels[3] = {
         bno1.getVector(Adafruit_BNO055::VECTOR_GYROSCOPE),
         bno2.getVector(Adafruit_BNO055::VECTOR_GYROSCOPE),
         bno3.getVector(Adafruit_BNO055::VECTOR_GYROSCOPE)
     };
 
+    // Retrieve attitude quaternions
     imu::Quaternion quats[3] = 
     {
         bno1.getQuat(),
@@ -46,6 +53,7 @@ void SensorStack::update()
         bno3.getQuat()
     };
 
+    // update all values
     for(int i = 0; i < 3; i++)
     {
         this->rollVel[i] = angVels[i].x();
@@ -63,9 +71,12 @@ void SensorStack::update()
     yAcc[1] = bno2.getVector(Adafruit_BNO055::VECTOR_ACCELEROMETER).y();
     yAcc[2] = bno3.getVector(Adafruit_BNO055::VECTOR_ACCELEROMETER).y();
 
+    // read and update altitude
+    bmp.performReading();
     altitude = bmp.readAltitude(this->seaLevelPressure);
 }
 
+// Getter functions for SensorStack class
 double * SensorStack::getPitchVel()
 {
     return this->pitchVel;
