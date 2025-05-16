f60e285388ca37d6c8076bc8b181e547b507ab93
Autumn Pan <pan662607@gmail.com>
2025-05-16 08:37:02 -0400

    add rocket methods

--- src/rocket/rocket.cpp
+++ src/rocket/rocket.cpp
@@ -7,3 +7,17 @@ Rocket::Rocket()
     this->altitude = 0;
     this->yVel = 0;
 }
+
+void Rocket::update(double altitude, double yVel, double yAcc)
+{
+    this->altitude = altitude;
+    this->yVel = yVel;
+    this->yAcc = yAcc;
+
+    this->time = millis();
+}
+
+unsigned int Rocket::getTime()
+{
+    return this->time;
+}
